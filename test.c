aa17143dc6191be64bb78d0b9a77b526219d57ef
Vivek Kumar Gupta <39339174+VivekKrG@users.noreply.github.com>
2020-02-15 21:35:13 +0530

    Delete test.c

--- test.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-void main(){
-    printf("hello bro\n\n");
-    char s[]={0xeb,0x2D,0x77, 0x3,'\0'};
-    char c[]={'a','x','g','l','\n'};
-    int ar[]={10,20,30};
-    printf("\n\t\a\a\a%d  \a\a  \n\n\n",ar);
-}
